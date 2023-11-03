b98f34a7489261470bfbea9f89751892f7bd4147
Neel <nkachalia1@gmail.com>
2023-11-03 14:22:05 -0400

    phase 3

--- practice-for-ch-react-thunk-pokedex-long-practice-main/src/components/PokemonDetail.js
+++ practice-for-ch-react-thunk-pokedex-long-practice-main/src/components/PokemonDetail.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PokemonItems from './PokemonItems';
 import EditPokemonForm from './EditPokemonForm';
 import ItemForm from './ItemForm';
+import { getPokemonDetail } from '../store/pokemon';
+
 
 const PokemonDetail = () => {
   const { pokemonId } = useParams();
@@ -11,7 +13,9 @@ const PokemonDetail = () => {
   const [showEditPokeForm, setShowEditPokeForm] = useState(false);
   const [editItemId, setEditItemId] = useState(null);
 
+  const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(getPokemonDetail(pokemonId), [pokemonId]);
     setShowEditPokeForm(false);
     setEditItemId(null);
   }, [pokemonId]);
@@ -24,16 +28,16 @@ const PokemonDetail = () => {
 
   if (editItemId) {
     content = (
-      <ItemForm 
-        itemId={editItemId} 
-        hideForm={() => setEditItemId(null)} 
+      <ItemForm
+        itemId={editItemId}
+        hideForm={() => setEditItemId(null)}
       />
     );
   } else if (showEditPokeForm && pokemon.captured) {
     content = (
-      <EditPokemonForm 
-        pokemon={pokemon} 
-        hideForm={() => setShowEditPokeForm(false)} 
+      <EditPokemonForm
+        pokemon={pokemon}
+        hideForm={() => setShowEditPokeForm(false)}
       />
     );
   } else {
@@ -66,7 +70,7 @@ const PokemonDetail = () => {
         </div>
         <div>
           <h2>
-            Items 
+            Items
             <button> + </button>
           </h2>
           <table>
--- practice-for-ch-react-thunk-pokedex-long-practice-main/src/store/pokemon.js
+++ practice-for-ch-react-thunk-pokedex-long-practice-main/src/store/pokemon.js
@@ -19,6 +19,15 @@ const addOnePokemon = pokemon => ({
   pokemon
 });
 
+export const getPokemonDetail = (id) => async dispatch => {
+  const response = await fetch(`/api/pokemon/${id}`)
+
+  if (response.ok) {
+    const singlePokemon = await response.json();
+    dispatch(addOnePokemon(singlePokemon));
+  }
+}
+
 export const getPokemon = () => async dispatch => {
   const response = await fetch(`/api/pokemon`);
 
@@ -50,7 +59,7 @@ const sortList = (list) => {
 
 const pokemonReducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOAD: 
+    case LOAD:
       const allPokemon = {};
       action.list.forEach(pokemon => {
         allPokemon[pokemon.id] = pokemon;
@@ -60,12 +69,12 @@ const pokemonReducer = (state = initialState, action) => {
         ...state,
         list: sortList(action.list)
       };
-    case LOAD_TYPES: 
+    case LOAD_TYPES:
       return {
         ...state,
         types: action.types
       };
-    case ADD_ONE: 
+    case ADD_ONE:
       if (!state[action.pokemon.id]) {
         const newState = {
           ...state,
@@ -83,7 +92,7 @@ const pokemonReducer = (state = initialState, action) => {
           ...action.pokemon
         }
       };
-    case LOAD_ITEMS: 
+    case LOAD_ITEMS:
       return {
         ...state,
         [action.pokemonId]: {
